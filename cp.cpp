b0f2fa5fa13600dc6a62aa0a699a252eef9f10f6
あむねしあ <147463615+shiorason@users.noreply.github.com>
2023-10-19 12:56:43 +0900

    Update cp.cpp

--- cp.cpp
+++ cp.cpp
@@ -5,17 +5,9 @@ using namespace std;
 int main()
 {
     string name;
-    int tosi;
 
     cout << "誰だお前は！" << endl;
     cin >> name;
 
-    cout << "ふむ、、" << name << "というんだな！" << endl;
-
-    cout << "ところで、" << name << "さん。失礼ですがお年はいくつですか？" << endl;
-    cout << "(入力はかならず、半角の数字でお願いします。)" << endl;
-    cin >> tosi;
-
-    cout << "なるほど。" << tosi << "歳ですか。" << endl;
-    cout << "私はもうすぐ2歳のマシンです。"<<endl;
+    cout << "ふむ、、" << name << "、というんだな！" << endl;
 }
