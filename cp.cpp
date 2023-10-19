73727fde2e8aa3801f6480842b7398be179506c8
あむねしあ <147463615+shiorason@users.noreply.github.com>
2023-10-19 12:19:48 +0900

    Update cp.cpp

--- cp.cpp
+++ cp.cpp
@@ -7,15 +7,14 @@ int main()
     string name;
     int tosi;
 
-    cout << "こんにちは。私はコンピュータです。" << endl;
-    cout << "あなたの名前を入力してください。" << endl;
-    cin >> name;      //名前（文字列）の入力
+    cout << "誰だお前は！" << endl;
+    cin >> name;
 
-    cout << name << "さん。よろしく。" << endl;
+    cout << "ふむ、、" << name << "というんだな！" << endl;
 
     cout << "ところで、" << name << "さん。失礼ですがお年はいくつですか？" << endl;
     cout << "(入力はかならず、半角の数字でお願いします。)" << endl;
-    cin >> tosi;      //年齢（整数）の入力
+    cin >> tosi;
 
     cout << "なるほど。" << tosi << "歳ですか。" << endl;
     cout << "私はもうすぐ2歳のマシンです。"<<endl;
