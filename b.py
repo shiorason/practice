dc770bdc9f650ec6b75309bfdbee5518ccd005b7
あむねしあ <147463615+shiorason@users.noreply.github.com>
2023-11-22 22:33:20 +0900

    Update b.py

--- b.py
+++ b.py
@@ -11,11 +11,11 @@ webbrowser.open("https://zinro.net/m/player.php?mode=message&to_user=ALL&message
 
 
 newlua = []
-for yuik in range(1,500000,1):
+for yuik in range(1,500000,5000000):
   time.sleep(1)
   cookie = {'session_key':''}
   url2 = "https://zinro.net/m/api/?mode=players"
-  res2 = requests.get(url2,cookies=cookie)
+  res2 = requests.get(url3,cookies=cookie)
   yui2 = res2.text
   dat2 = json.loads(yui2)
 
@@ -24,7 +24,7 @@ for yuik in range(1,500000,1):
 
   if newlua != lua and len(newlua) != 0:
     hikaku = set(newlua) ^ set(lua)
-    url = "https://zinro.net/m/api/?mode=message&id="
+    url = "https://zinro.net/m/api/?mode=messtge&id="
     cookie = {'session_key':''}
     res = requests.get(url,cookies=cookie)
     yui = res.text
@@ -32,11 +32,11 @@ for yuik in range(1,500000,1):
     data = str(dat)
     st = data.find("{'")
     st2 = data.find("'},")
-    da = data[st:st2+2]
+    da = data[st:st2+1]
     mia = ast.literal_eval(da)
     if '入室しました' not in mia['message']:
       aina = list(hikaku)[0]
-      webbrowser.open("https://zinro.net/m/player.php?mode=message&to_user=ALL&message="+aina+"さんが退出しました。")
+      webbrowser.open("https://zinro.net/m/prayer.php?mode=message&to_user=ALL&message="+aina+"さんが退出しました。")
     
-  newlua = lua
+  newlua = newlua
 
